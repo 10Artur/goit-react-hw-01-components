a92bf9f6e91dfb87dcc19361821385384b30f1e5
10Artur <artyr11012002@gmail.com>
2023-08-03 22:54:44 +0300

    create

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,29 @@
+import {
+  StatisticsItem,
+  StatisticsLabel,
+  StatisticsList,
+  StatisticsPercentage,
+  StatisticsSection,
+  StatisticsTitle,
+} from './Statistics.styled';
+
+function randomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <StatisticsSection>
+      {title && <StatisticsTitle> {title} </StatisticsTitle>}
+
+      <StatisticsList>
+        {stats.map(({ id, label, percentage }) => (
+          <StatisticsItem key={id} color={randomColor()}>
+            <StatisticsLabel>{label}</StatisticsLabel>
+            <StatisticsPercentage>{percentage}%</StatisticsPercentage>
+          </StatisticsItem>
+        ))}
+      </StatisticsList>
+    </StatisticsSection>
+  );
+};
