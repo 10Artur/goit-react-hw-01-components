6fc3f587515680b0ec2beba0ab9e36b2e89a82c1
10Artur <artyr11012002@gmail.com>
2023-08-05 22:38:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { Profile } from './Profile/Profile';
-import userData from '../user.json';
+import userData from '../data/user.json';
 import { Statistics } from './Statistics/Statistics';
-import data from '../data.json';
+import data from '../data/data.json';
 import { FriendsList } from './FriendList/FriendList';
-import friendsData from '../friends.json';
+import friendsData from '../data/friends.json';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import transactions from '../transactions.json';
+import transactions from '../data/transactions.json';
 import { GlobalStyle } from 'components/GlobalStyle';
 
 export const App = () => {
